d395335419e447e44d7fe9d7da3e5d209e9ec49b
Marko Budal <markobudal@gmail.com>
2022-03-26 20:20:11 +0100

    first commit

---
+++ AVKitPlayer/AVKitPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVKitPlayerApp.swift
+//  AVKitPlayer
+//
+//  Created by Marko Budal on 26/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct AVKitPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
